d4941166b96c163969082b77e903aafaf462949e
NikitaT206 <acid3602@yandex.ru>
2022-04-01 01:23:36 +0300

    feat: perfomance and different font-size

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-let width = window.innerWidth
-let height = window.innerHeight
+'use strict'
 
-const arrayOfNumbers = [...new Array(Math.floor(height / 20))]
-const arrayOfArrays = [...new Array(Math.floor(width / 10))]
+const height = window.innerHeight
+const width = window.innerWidth
+
+function getArrayOfNumbers() {
+  let n
+  if (width < 1279) n = 40
+  if (width < 1023) n = 30
+  if (width < 767) n = 20
+  if (width < 400) n = 15
+  const arrayOfNumbers = [...new Array(Math.floor(height / n))]
+  return arrayOfNumbers
+}
+
+function getArrayOfArrays() {
+  let n
+  if (width < 1279) n = 40
+  if (width < 1023) n = 30
+  if (width < 767) n = 20
+  if (width < 400) n = 15
+  const arrayOfArrays = [...new Array(Math.floor(width / n))]
+  return arrayOfArrays
+}
 
 function getRandomNumber() {
   let randomNumber = Math.floor(Math.random() * 10)
@@ -12,17 +31,16 @@ function getRandomNumber() {
 function createArray() {
   const array = document.createElement('div')
   array.classList.add('array')
-  arrayOfNumbers.forEach(() => {
+  getArrayOfNumbers().forEach(() => {
     const number = document.createElement('div')
     number.classList.add('number')
-    number.textContent = getRandomNumber()
     array.prepend(number)
   })
   return array
 }
 
 function createArrayOfArrays() {
-  arrayOfArrays.forEach(() => {
+  getArrayOfArrays().forEach(() => {
     document.body.insertAdjacentHTML('afterbegin', createArray().outerHTML)
   })
 }
@@ -32,41 +50,55 @@ createArrayOfArrays()
 const arrays = document.querySelectorAll('.array')
 
 arrays.forEach((array) => {
-  let time = Math.floor(Math.random() * 10000 + 5000)
-  let delay = Math.floor(Math.random() * 200 + 50)
+  let time = Math.floor(Math.random() * 4000)
+  let delay = Math.floor(Math.random() * 150 + 50)
+  let transitionDelay = Math.floor(Math.random() * 10)
 
   setInterval(() => {
-    time = Math.floor(Math.random() * 10000 + 5000)
-    delay = Math.floor(Math.random() * 200 + 50)
+    time = Math.floor(Math.random() * 4000 + 2500)
+    delay = Math.floor(Math.random() * 150 + 50)
+    transitionDelay = Math.floor(Math.random() * 1000 + 200)
   }, time)
 
   const numbers = array.childNodes
 
-  for (let i = 0; i < numbers.length; i++) {  
-    setInterval(() => {
-      setTimeout(() => {
-        numbers[i].style.color = `rgb(100, 255, 100)`
-        numbers[i].style.textShadow = `0 0 5px greenyellow`
-        numbers[i].style.transition = 'color 0s'
-        numbers[i].textContent = getRandomNumber()
-        setTimeout(() => {
-          numbers[i].style.textShadow = `none`
-          numbers[i].style.color = `black`
-          numbers[i].style.transition = 'color 1s linear'
-        }, delay)
-      }, i * delay)
-    }, time)
+  for (let i = 0; i < numbers.length; i++) {
+    function go() {
+      const interval = setInterval(() => {
+        one()
+
+        function one() {
+
+          const timeout = setTimeout(() => {
+            numbers[i].style.color = `rgb(100, 255, 100)`
+            numbers[i].style.textShadow = `0 0 5px greenyellow`
+            numbers[i].style.transition = '0s'
+            numbers[i].textContent = getRandomNumber()
+            clearTimeout(timeout)
+            two()
+          }, i * delay)
+
+        function two() {
+
+          const timeout3 = setTimeout(() => {
+            numbers[i].style.textShadow = 'none'
+            numbers[i].style.color = 'rgb(40, 255, 40)'
+            clearInterval(timeout3)
+          }, delay)
+
+          const timeout2 = setTimeout(() => {
+            numbers[i].style.transition = `color ${transitionDelay}ms linear`
+            numbers[i].style.color = 'black'
+            clearTimeout(timeout2)
+          }, delay + 1000)
+        }
+      }
+
+      clearInterval(interval)
+      go()
+      }, time)
+    }
+    go()
   }
 })
-
-
-
-
-
-
-
-
-
-
-
 
